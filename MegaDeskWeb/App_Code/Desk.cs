da70e4907922401952ecc9da1d6c9aa9f48a5b3e
danverde <gre12014@byui.edu>
2018-06-19 08:42:49 -0600

    initial commit

---
+++ MegaDeskWeb/App_Code/Desk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk4
+{
+    class Desk
+    {
+        public enum Surface
+        {
+            Laminate = 100,
+            Oak = 200,
+            Pine = 50,
+            Rosewood = 300,
+            Veneer = 125
+        }
+
+        public decimal Depth { get; set; }
+
+        public decimal Width { get; set; }
+
+        public int NumDrawers { get; set; }
+
+        public int SurfaceMaterial { get; set; }
+    }
+}
